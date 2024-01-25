7b942519edda92d6fd916162e42b2abeac140b22
Jay <jayjethava.tst@gmail.com>
2024-01-25 12:17:31 +0530

    express.json() middleware added

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,10 @@ const app = express();
 const port = 3001;
 
 app.use(cors());
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.post("/testing/firebase/auth", socialAuth);
+
 app.use("/", (req, res) => res.send("Welcome..."));
 
 app.listen(port, () => {
