b1ab2fad8e615fba2bba725a7a96ab3338aaba5f
Jay <jayjethava.tst@gmail.com>
2024-01-25 12:43:17 +0530

    dot env added, socialAuth modified

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -1,4 +1,6 @@
 const firebase = require("firebase-admin");
+const jwt = require("jsonwebtoken");
+const createError = require("http-errors");
 
 exports.socialAuth = async (req, res, next) => {
   console.log("payload", req.body);
@@ -14,24 +16,14 @@ exports.socialAuth = async (req, res, next) => {
           createError(401, "Email is not verified, Try with another email.")
         );
 
-      let [user] = await userService.get({
-        where: { email: firebaseUser.email },
-      });
-
-      console.log(user);
-
-      if (!user)
-        user = await userService.create({
-          email: firebaseUser.email,
-          name: firebaseUser.name,
-        });
+      // Fetch user from DB with required attributes to sign login token
 
       // Sign a JWT Token as Login Token
       const token = jwt.sign(
         {
           // id: user.id,
           // email: user.email,
-          ...user.toJSON(),
+          email: firebaseUser.email,
           role: "user",
         },
         process.env.JWT_SECRET
@@ -40,6 +32,7 @@ exports.socialAuth = async (req, res, next) => {
       return res.status(200).json({
         status: "success",
         message: "Login successful",
+        token,
       });
     })
     .catch((err) => next(err));
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,7 @@
+const dotenv = require("dotenv");
+dotenv.config({
+  path: "./.env",
+});
 const express = require("express");
 const cors = require("cors");
 const middleware = require("./middleware");
